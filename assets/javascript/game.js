3d5160c2f7156c49f3bc752c61d47e7248d4ae6f
Jose Torres <jt227b@gmail.com>
2019-02-28 00:26:02 -0600

    Updating JS&JQ

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,26 @@
 /* The random number shown at the start of the game should be between 19 - 120.
 Each crystal should have a random hidden value between 1 - 12.    */
 
+
+
+
+// Variable for the Game//
+var randomNumbers = 0;
+var wins = 0;
+var losses = 0;
+var totalScores = 0;
+
+// Variables for Characters //
+var crystalMike = 0;
+var crystalJesse = 0;
+var crystalWhite = 0;
+var crystalSaul = 0; 
+
+// Random Functions //
+
+function initializeGame(){
+    randomNumbers = Math.floor(Math.random() * 120) + 10;
+    
+}
+
+
